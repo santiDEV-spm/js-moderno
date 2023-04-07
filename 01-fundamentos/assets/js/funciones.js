66b34718d1368b2036c173c4d48380cd662e1f4c
santiDev <androidsan94@gmail.com>
2023-04-07 17:47:27 -0600

    Second functions

--- 01-fundamentos/assets/js/funciones.js
+++ 01-fundamentos/assets/js/funciones.js
@@ -1,6 +1,7 @@
 function saludar(nombre){
     console.log(arguments);
     console.log('Hola ' + nombre);
+    return 1;
 }
 
 const saludar2 = function (nombre){
@@ -14,7 +15,27 @@ const saludar4 = (nombre) => {
     console.log('Hola ' + nombre);
 }
 
-saludar('Santiago', 22, true, 'Michoacan');
-saludar2('Yaneth');
+const retornoSaludar = saludar('Santiago', 22, true, 'Michoacan');
+console.log(retornoSaludar);
 saludar3('Alejandra');
 saludar4('Iker');
+
+function sumar(a,b){
+    return a + b;
+}
+
+const sumar2 = (a,b) =>{
+    return a+b;
+};
+
+const sumar3 = (a,b) => a+b;
+
+function getAleatorio(){
+    return Math.random();
+}
+
+let getAleatorio2 = () => Math.random();
+
+console.log(sumar(3,5));
+console.log(getAleatorio());
+console.log(getAleatorio2());
